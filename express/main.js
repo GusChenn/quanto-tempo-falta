2b033b09a062659c463d638aed2fe8f453169cc6
Gustavo Donnini Chen <gustavo.sao@gmail.com>
2022-11-24 00:30:01 -0300

    Create visuals

--- express/main.js
+++ express/main.js
@@ -1,5 +1,20 @@
 const hoje = Date.now();
-const formatura = new Date("2022-12-22").getTime();
+const formatura = new Date('2022-12-22').getTime();
+
+const timeCalculator = (now, targetDate) => {
+  let delta = Math.abs(targetDate - now) / 1000;
+  const days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+  const hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+  const minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
+  const seconds = delta % 60;
+
+  return [
+    toString(days), toString(hours), toString(minutes), toString(seconds.toFixed(0)),
+  ];
+};
 
 let delta = Math.abs(formatura - hoje) / 1000;
 const dias = Math.floor(delta / 86400);
@@ -10,14 +25,14 @@ const minutos = Math.floor(delta / 60) % 60;
 delta -= minutos * 60;
 const segundos = delta % 60;
 
-const textoDias = `${dias} dias`;
-const textoHoras = `${horas} horas`;
-const textoMinutos = `${minutos} minutos`;
-const textoSegundos = `${segundos.toFixed(0)} segundos`;
+const textoDias = `${dias}`;
+const textoHoras = `${horas}`;
+const textoMinutos = `${minutos}`;
+const textoSegundos = `${segundos.toFixed(0)}`;
 const conteudos = [textoDias, textoHoras, textoMinutos, textoSegundos];
 
-const quantityContainer = document.getElementsByClassName("quantityContainer");
+const quantityContainer = document.getElementsByClassName('quantityContainer');
 
 conteudos.forEach((conteudo, index) => {
-  quantityContainer.item(index).append(conteudo)
-})
+  quantityContainer.item(index).append(conteudo);
+});
