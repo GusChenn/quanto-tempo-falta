eba769bae0699ac353f06993a2b101fa7ace5108
Gustavo Donnini Chen <gustavo.sao@gmail.com>
2022-11-24 00:29:37 -0300

    Eslint fixes

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require("express");
-const path = require("path");
-const http = require("http");
+const express = require('express');
+const path = require('path');
 
-const SPApath = "/express/";
+const SPApath = '/express/';
 const port = 8090;
 
 const app = express();
 app.use(express.json());
-app.use(express.static("express"));
+app.use(express.static('express'));
 
-app.use("/", (req, res) => {
+app.use('/', (req, res) => {
+  // eslint-disable-next-line no-path-concat
   res.sendFile(path.join(__dirname + SPApath));
 });
 
 app.listen(port, (err) => {
-  if (err) console.log("Error in server setup")
-  console.log(`Listening on port ${port}`)
-})
-
+  if (err) console.log('Error in server setup');
+  console.log(`Listening on port ${port}`);
+});
